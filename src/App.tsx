de266ad2102996d9610a77dabb2ab582abe69600
kgarayev <kenangarayev@gmail.com>
2023-08-25 18:52:02 +0100

    mode added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
   const [mode, setMode] = useState(0);
 
   console.log("check");
+
+  setMode(1);
+
+  console.log(mode);
 
   return (
     <>
@@ -32,7 +36,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
