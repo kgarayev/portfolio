a162e27db49356e31761ef2c4204646eeac80234
kgarayev <kenangarayev@gmail.com>
2023-08-27 22:49:23 +0100

    store created

---
+++ store/initialState.ts
@@ -0,0 +1,54 @@
+// create initial state
+export const initialState = {
+  registerInput: {
+    firstName: "",
+    lastName: "",
+    number: "",
+    email: "",
+    dob: "",
+    password: "",
+    confirmPassword: "",
+  },
+  errors: null,
+  loginInput: {
+    email: "",
+    password: "",
+  },
+  screenMode: 0,
+
+  payInput: {
+    payeeName: "",
+    sortCode: "",
+    accountNumber: "",
+  },
+
+  passwordChange: {
+    currentPassword: "",
+    newPassword: "",
+    confirmNewPassword: "",
+  },
+
+  debitInput: {
+    cardNumber: "",
+    expiryDate: "",
+    CVV: "",
+  },
+
+  account: {
+    holderName: "",
+    firstName: "",
+    lastName: "",
+    name: "",
+    dob: "",
+    phoneNumber: "",
+    accountNumber: "",
+    password: "",
+    sortCode: "",
+    currencyCode: "",
+    currencyName: "",
+    currencySymbol: "",
+    currencyCountry: "",
+    balance: "",
+    transactions: [],
+  },
+};
---
+++ store/mainSlice.ts
@@ -0,0 +1,84 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { initialState } from "./initialState";
+
+// export the rducer functions
+export const mainSlice = createSlice({
+  name: "main",
+  initialState,
+
+  reducers: {
+    // save the registration input values
+    setRegisterInput: (state, action) => {
+      state.registerInput = action.payload;
+    },
+
+    // record the joi validation errors
+    setErrors: (state, action) => {
+      state.errors = action.payload;
+    },
+
+    // save the registration input values
+    setLoginInput: (state, action) => {
+      state.loginInput = action.payload;
+    },
+
+    // set screen mode
+    setScreenMode: (state, action) => {
+      state.screenMode = action.payload;
+    },
+
+    // set account balance
+    setBalance: (state, action) => {
+      state.account.balance = action.payload;
+    },
+
+    // pay input
+    setPayInput: (state, action) => {
+      state.payInput = action.payload;
+    },
+
+    // debit card input
+    setDebitInput: (state, action) => {
+      state.debitInput = action.payload;
+    },
+
+    // change password
+    changePassword: (state, action) => {
+      state.passwordChange = action.payload;
+    },
+  },
+});
+
+// exporting the reducer functions
+export const {
+  setRegisterInput,
+  setErrors,
+  setLoginInput,
+  setScreenMode,
+  setPayInput,
+  setBalance,
+  setDebitInput,
+  changePassword,
+} = mainSlice.actions;
+
+// exporting the selectors
+export const selectRegisterInput = (state: { main: typeof initialState }) =>
+  state.main.registerInput;
+export const selectErrors = (state: { main: typeof initialState }) =>
+  state.main.errors;
+export const selectLoginInput = (state: { main: typeof initialState }) =>
+  state.main.loginInput;
+export const selectScreenMode = (state: { main: typeof initialState }) =>
+  state.main.screenMode;
+export const selectAccount = (state: { main: typeof initialState }) =>
+  state.main.account;
+export const selectTransactions = (state: { main: typeof initialState }) =>
+  state.main.account.transactions;
+export const selectPayInput = (state: { main: typeof initialState }) =>
+  state.main.payInput;
+export const selectDebitInput = (state: { main: typeof initialState }) =>
+  state.main.debitInput;
+export const selectPasswordChange = (state: { main: typeof initialState }) =>
+  state.main.passwordChange;
+
+export default mainSlice.reducer;
