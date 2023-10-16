d91ed01921d3ccb660b35aa7ae0d824c63a065f8
kgarayev <kenangarayev@gmail.com>
2023-10-16 20:54:00 +0100

    font played with

--- src/components/Interface.tsx
+++ src/components/Interface.tsx
@@ -3,7 +3,7 @@ import { Routes, Route, useNavigate } from "react-router-dom";
 const Interface = () => {
   return (
     <>
-      <div>
+      <div className={"font-ibmPlexMono"}>
         Hello World
         {/* includes routes here  */}
       </div>
