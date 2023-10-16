c514b1db1545a551709a1ac0d1c75b147fdada45
kgarayev <kenangarayev@gmail.com>
2023-10-16 20:33:55 +0100

    tidy up

---
+++ src/components/Interface.tsx
@@ -0,0 +1,23 @@
+import { Routes, Route, useNavigate } from "react-router-dom";
+
+const Interface = () => {
+  return (
+    <>
+      <div>
+        Hello World
+        {/* includes routes here  */}
+      </div>
+
+      {/* <Routes>
+        <Route path="/" element={<Welcome />} />
+
+        <Route
+          path="/register"
+          element={<Template component={registerComponent} />}
+        />
+      </Routes> */}
+    </>
+  );
+};
+
+export default Interface;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
-export default function Home() {
+import Interface from "@/components/Interface";
+
+const Home = () => {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24 bg-white`}
     >
-      <div className={`border border-black border-0.1`}>HELLO</div>
+      <Interface />
     </main>
   );
-}
+};
+
+export default Home;
