af351ac4ab187a502140f24b274d48892ddfcc4a
kgarayev <kenangarayev@gmail.com>
2023-10-20 20:46:26 +0100

    logo stuff

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -2,6 +2,7 @@ const Logo = () => {
   return (
     <div className="h-full">
       <img src="/5.gif" alt="logo" className="h-full" />
+      {/* <img src="/4.png" alt="logo" className="h-full" /> */}
     </div>
   );
 };
