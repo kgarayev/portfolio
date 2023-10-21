406782493827192249b87ab69e524def92b6c962
kgarayev <kenangarayev@gmail.com>
2023-10-21 23:09:34 +0100

    profile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,14 @@
 import Logo from "./Logo";
+import Photo from "./Photo";
 
 const Header = () => {
   return (
     <div className="flex justify-center w-full h-24 ">
       <div className="h-full">
         <Logo></Logo>
+      </div>
+      <div>
+        <Photo></Photo>
       </div>
     </div>
   );
--- src/components/Photo.tsx
+++ src/components/Photo.tsx
@@ -1,11 +1,13 @@
 const Photo = () => {
   return (
     <div className="h-full">
-      <img
-        src="/profile2.jpg"
-        alt="Circular Image"
-        className="w-24 h-24 rounded-full"
-      />
+      <div className="w-24 h-24 rounded-full overflow-hidden">
+        <img
+          src="/profile2.jpg"
+          alt="Circular Image"
+          className="w-full h-full transform scale-110 translate-y-1"
+        />
+      </div>
     </div>
   );
 };
