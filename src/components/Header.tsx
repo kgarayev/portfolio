3861204fc3cfdde7277a2d8949ea413295495c00
kgarayev <kenangarayev@gmail.com>
2023-10-22 00:57:27 +0100

    minor change

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Photo from "./Photo";
 
 const Header = () => {
   return (
-    <div className="flex items-center justify-center gap-4 p-2 w-full border border-gray-500">
+    <div className="flex items-center justify-center gap-4 p-1 w-full">
       <div className="h-full">
         <Logo></Logo>
       </div>
