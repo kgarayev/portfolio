eccd0533addf6c0bfa732220865a209e005ea06b
kgarayev <kenangarayev@gmail.com>
2023-10-22 00:56:02 +0100

    logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,7 @@ import Photo from "./Photo";
 
 const Header = () => {
   return (
-    <div className="flex flex-col items-center justify-center gap-4 p-2 w-full border border-gray-500">
+    <div className="flex items-center justify-center gap-4 p-2 w-full border border-gray-500">
       <div className="h-full">
         <Logo></Logo>
       </div>
--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,7 +1,9 @@
 const Logo = () => {
   return (
-    <div className="h-36">
-      <img src="/6.gif" alt="Trimmed Image" className="w-full h-full" />
+    <div className="h-40 w-40">
+      <div className="w-full h-full rounded-full overflow-hidden">
+        <img src="/6.gif" alt="Trimmed Image" className="w-full h-full" />
+      </div>
     </div>
   );
 };
