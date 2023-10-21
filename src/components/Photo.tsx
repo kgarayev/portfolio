b052d0a22c79c37c4036c6b391a1c879734d3d40
kgarayev <kenangarayev@gmail.com>
2023-10-21 23:05:02 +0100

    profile

---
+++ src/components/Photo.tsx
@@ -0,0 +1,13 @@
+const Photo = () => {
+  return (
+    <div className="h-full">
+      <img
+        src="/profile2.jpg"
+        alt="Circular Image"
+        className="w-24 h-24 rounded-full"
+      />
+    </div>
+  );
+};
+
+export default Photo;
