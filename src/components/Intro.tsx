f1f6abb9ad1b708c753f57a0ae9c414d9e18bb16
kgarayev <kenangarayev@gmail.com>
2023-10-23 20:50:33 +0100

    intro

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -1,9 +1,11 @@
 const Intro = () => {
   return (
     <div className="border p-5">
-      <p>Hi, my name is Kenan.</p>
-      <h1>I transform challenges into digital solutions.</h1>
-      <p>
+      <p className="mb-5">Hi, my name is Kenan.</p>
+      <h1 className="mb-5 text-2xl font-bold">
+        I transform challenges into digital solutions.
+      </h1>
+      <p className="">
         I'm a Software Engineer with a wealth of expertise in solving real-world
         challenges. With a strong foundation in computer science, my proficiency
         lies in utilising Node.js, React, Python, C, SQL and NoSQL for backend
