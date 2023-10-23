72ff3d052cfad4d92b47afbd8be8ba059e9b2b3d
kgarayev <kenangarayev@gmail.com>
2023-10-23 20:39:39 +0100

    intro

--- src/components/MainTemplate.tsx
+++ src/components/MainTemplate.tsx
@@ -1,4 +1,5 @@
 import Header from "./Header";
+import Intro from "./Intro";
 import StickyFooter from "./StickyFooter";
 import Work from "./Work";
 
@@ -6,6 +7,7 @@ const MainTemplate = () => {
   return (
     <div className="w-full">
       <Header></Header>
+      <Intro></Intro>
       <Work></Work>
       <StickyFooter></StickyFooter>
     </div>
