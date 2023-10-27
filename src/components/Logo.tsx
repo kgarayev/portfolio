61c871fefc6fa447d94dadf3be17a2cb31630d9d
kgarayev <kenangarayev@gmail.com>
2023-10-28 00:05:18 +0100

    logo

--- src/components/Logo.tsx
+++ src/components/Logo.tsx
@@ -1,11 +1,12 @@
+import Typewriter from "react-ts-typewriter";
+
 const Logo = () => {
   return (
-    <div className="relative">
+    <div className="relative h-32">
       {/* This is the main text */}
       <h1 className="relative z-10 text-5xl text-blue-600 font-ultra font-thin">
-        Kenan
+        <Typewriter text="Kenan" loop={true} speed={200} delay={200} />
       </h1>
-
       {/* This is the shadow */}
       <h1 className="absolute top-[3px] left-[5px] text-5xl font-ultra font-bold">
         Kenan
