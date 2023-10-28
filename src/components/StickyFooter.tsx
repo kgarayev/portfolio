4405c99f44a4d3f18412efa5765c240470c70a4b
kgarayev <kenangarayev@gmail.com>
2023-10-29 00:03:10 +0100

    footer

--- src/components/StickyFooter.tsx
+++ src/components/StickyFooter.tsx
@@ -8,7 +8,7 @@ const StickyFooter = () => {
           viewBox="0 0 24 24"
           strokeWidth={1.5}
           stroke="currentColor"
-          className="w-5 h-5"
+          className="w-8 h-8"
         >
           <path
             strokeLinecap="round"
