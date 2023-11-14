73ad0c2995fae1e044e71b720fd34f3fb106d905
kgarayev <kenangarayev@gmail.com>
2023-11-14 21:38:50 +0000

    change

--- src/components/Intro.tsx
+++ src/components/Intro.tsx
@@ -2,11 +2,11 @@ import Photo from "./Photo";
 
 const Intro = () => {
   return (
-    <div className="p-5 pt-0 pb-16">
+    <div className="p-5 pt-0 pb-8">
       <h1 className="mb-5 text-2xl font-semibold mt-2">
         I am a Software Engineer.
       </h1>
-      <p className="mb-5 font-nunito font-semibold mt-6">
+      <p className="mb-2 font-nunito font-semibold mt-6">
         Building things with code to solve real-world problems is what I enjoy
         most. Skilled in{" "}
         <span className=" text-xl font-bold font-ibmPlexMono  bg-emerald-200">
