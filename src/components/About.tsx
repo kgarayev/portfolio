ae9ade0e68cee91474185e9dc558c78daec6f995
kgarayev <kenangarayev@gmail.com>
2023-11-21 22:56:10 +0000

    gh

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,55 +9,67 @@ const About = forwardRef<HTMLDivElement>((props, ref) => {
         <h2 className="text-xl font-extrabold">About me</h2>
       </div>
 
-      <div className="pl-5 pr-5 w-full ">
-        <div className="relative max-w-[450px] max-h-[450px] mx-auto">
-          <div className="absolute top-1.5 left-1 bg-gray-700 rounded-2xl shadow-md aspect-w-1 aspect-h-1 w-full max-w-3xl overflow-hidden "></div>
-          <div className="aspect-w-1 aspect-h-1 w-full max-w-3xl overflow-hidden relative border-2 relative z-10 bg-white border-2 border-gray-700 rounded-2xl neo-brutalist transition-transform">
-            <Image
-              src="/photo.jpg"
-              alt="Square Image"
-              className="min-w-full min-h-full absolute bottom-0 left-0 object-cover"
-              width={1000}
-              height={1000}
-            />
+      <div className="2md:flex">
+        <div className="pl-5 pr-5 w-full 2md:flex-grow-1 border">
+          <div className="relative min-w-[300px] max-w-[450px] max-h-[450px]">
+            <div className="absolute top-1.5 left-1 bg-gray-700 rounded-2xl shadow-md aspect-w-1 aspect-h-1 w-full max-w-3xl overflow-hidden "></div>
+            <div className="aspect-w-1 aspect-h-1 w-full max-w-3xl overflow-hidden relative border-2 relative z-10 bg-white border-2 border-gray-700 rounded-2xl neo-brutalist transition-transform">
+              <Image
+                src="/photo.jpg"
+                alt="Square Image"
+                className="min-w-full min-h-full absolute bottom-0 left-0 object-cover"
+                width={1000}
+                height={1000}
+              />
+            </div>
+          </div>
+        </div>
+
+        <div className="2md:flex-grow-1 border w-full">
+          <div className="p-5 pb-0 mb-0 font-nunito ">
+            <p className="mb-4">
+              Hey, I&apos;m Kenan Garayev. I&apos;m an experienced software
+              developer and an ex-civil engineer.
+            </p>
+            <p className="mb-4">
+              In the realm of software, my skill set is extensive. I excel in
+              frontend and backend development and devops engineering.
+            </p>
+            {/* <p className="mb-8">
+              My path to software engineering was anything but linear. Starting
+              off with a degree from UCL, I became a Chartered Engineer and
+              project manager in civil engineering, accumulating over 7 years of
+              experience. However, my passion shifted to software engineering,
+              moving away from managing big construction projects. My early
+              school days dabbling in Pascal, C, and Python, followed by
+              intensive Bootcamp training, led me back to solving
+              construction-related problems with new tech insights.{" "}
+            </p> */}
           </div>
         </div>
       </div>
 
-      <div className="p-5 font-nunito">
-        <p className="mb-8">
-          Hey, I&apos;m Kenan Garayev. I&apos;m an ex-civil engineer turned
-          software developer.
-        </p>
-        <p className="mb-8">
-          My path to software engineering was anything but linear. Starting off
-          with a degree from UCL, I became a Chartered Engineer and project
-          manager in civil engineering, accumulating over 7 years of experience.
-          However, my passion shifted to software engineering, moving away from
-          managing big construction projects. My early school days dabbling in
-          Pascal, C, and Python, followed by intensive Bootcamp training, led me
-          back to solving construction-related problems with new tech insights.{" "}
-        </p>
-        <p className="mb-8">
+      <div className="p-5 pt-0 mt-0 font-nunito">
+        {/* <p className="mb-8">
           In the realm of software, my skill set is extensive. I excel in both
           frontend and backend development, showing particular expertise in
           TypeScript, JavaScript, Node.js, React, and Python. Additionally,
           I&apos;m proficient in managing both SQL and NoSQL databases. I&apos;m
           all about making sure every line of code I write not only works but
           works well and lasts.
-        </p>
-        <p className="mb-8">
+        </p> */}
+        {/* <p className="mb-8">
           At the Keller Group, I&apos;ve utilised these skills to enhance our
           projects and streamline operations. Yet, being a software engineer to
           me means more than coding; it&apos;s about mentoring junior engineers
           and contributing to their growth. Sharing knowledge, teaching, and
           coaching are as vital to me as building innovative software.
-        </p>
-        <p className="mb-8">
+        </p> */}
+        <p className="mb-4">
           When I&apos;m not coding, I love to share what I&apos;ve learned with
           people.
         </p>
-        <p className="mb-8">
+        <p className="mb-6">
           I love good design, hiking camping, travel and music.
         </p>
         <h5 className="font-semibold text-lg mb-4 font-ibmPlexMono">
