fb6b3fd1076ef7416f1aaf8a60e46c0965c1fec1
kgarayev <kenangarayev@gmail.com>
2023-11-22 19:08:47 +0000

    kj

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,27 +3,32 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <title>Kenan Garayev</title>
+      <Head>
+        <title>Kenan Garayev</title>
+        <meta property="og:title" content="Kenan Garayev" />
+        <meta
+          property="og:description"
+          content="Kenan Garayev's Portfolio Site"
+        />
+        <meta
+          property="og:image"
+          content="http://kgarayev.netlify.app/thumbnail.png"
+        />
+        <meta property="og:url" content="http://kgarayev.netlify.app" />
+        <meta property="og:type" content="website" />
 
-      <meta property="og:title" content="Kenan Garayev" />
-      <meta
-        property="og:description"
-        content="Kenan Garayev's Portfolio Site"
-      />
-      <meta property="og:image" content={"/thumbnail.png"} />
-      <meta property="og:url" content="http://kgarayev.netlify.app" />
-      <meta property="og:type" content="website" />
-
-      {/* Twitter Card tags */}
-      <meta name="twitter:card" content="summary_large_image" />
-      <meta name="twitter:title" content="Kenan Garayev" />
-      <meta
-        name="twitter:description"
-        content="Kenan Garayev's Portfolio Site"
-      />
-      <meta name="twitter:image" content="http://kgarayev.netlify.app" />
-
+        {/* Twitter Card tags */}
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Kenan Garayev" />
+        <meta
+          name="twitter:description"
+          content="Kenan Garayev's Portfolio Site"
+        />
+        <meta
+          name="twitter:image"
+          content="http://kgarayev.netlify.app/twitter-image.png"
+        />
+      </Head>
       <body className="">
         <Main />
         <NextScript />
