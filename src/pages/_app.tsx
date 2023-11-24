c803b7534c31d36722ce77e7a7b794ba0e167770
kgarayev <kenangarayev@gmail.com>
2023-11-24 12:41:36 +0000

    jh

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,34 @@ import "@/styles/globals.css";
 import { Provider } from "react-redux";
 import { store } from "../store/store";
 import type { AppProps } from "next/app";
+import ReactGA from "react-ga4";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Initialize Google Analytics
+    ReactGA.initialize("G-87XFY2X9NB");
+
+    // Function to track page views
+    const trackPageViews = (url: string) => {
+      ReactGA.send({ hitType: "pageview", page: url });
+    };
+
+    // Track the initial pageview
+    trackPageViews(router.asPath);
+
+    // Add the event listener for tracking subsequent pageviews
+    router.events.on("routeChangeComplete", trackPageViews);
+
+    // Remove the event listener when the component unmounts
+    return () => {
+      router.events.off("routeChangeComplete", trackPageViews);
+    };
+  }, [router.events, router.asPath]);
+
   return (
     <Provider store={store}>
       <Component {...pageProps} />
