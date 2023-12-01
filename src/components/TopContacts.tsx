bbc0c163acf3825f65b8fad686af6a67a088178f
kgarayev <kenangarayev@gmail.com>
2023-12-01 18:04:09 +0000

    number

--- src/components/TopContacts.tsx
+++ src/components/TopContacts.tsx
@@ -4,8 +4,11 @@ const TopContacts = () => {
   return (
     <>
       <div className="flex gap-2">
-        <div className="border h-12 w-full max-w-32 rounded-md">
-          <a href={`tel:${phoneNumber}`}>Call Me</a>
+        <div className="border h-12 w-full max-w-32 rounded-md flex justify-center items-center flex-col text-sm">
+          <a href={`tel:${phoneNumber}`} className="">
+            call me
+          </a>
+          <button>copy number</button>
         </div>
         <div className="border h-12 w-full max-w-32 rounded-md">World</div>
       </div>
