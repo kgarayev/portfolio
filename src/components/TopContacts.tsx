ec597ccc59baaea7382310ee73a5acc43225096b
kgarayev <kenangarayev@gmail.com>
2023-12-01 18:01:22 +0000

    phone

--- src/components/TopContacts.tsx
+++ src/components/TopContacts.tsx
@@ -1,9 +1,13 @@
 const TopContacts = () => {
+  const phoneNumber = "+447472219991";
+
   return (
     <>
       <div className="flex gap-2">
-        <div className="border h-12 w-full max-w-32">Hello</div>
-        <div className="border h-12 w-full max-w-32">World</div>
+        <div className="border h-12 w-full max-w-32 rounded-md">
+          <a href={`tel:${phoneNumber}`}>Call Me</a>
+        </div>
+        <div className="border h-12 w-full max-w-32 rounded-md">World</div>
       </div>
     </>
   );
