ad2c2cf24d96a7f9d44fad55edc13620b5d33383
kgarayev <kenangarayev@gmail.com>
2023-12-01 21:14:19 +0000

    resume

---
+++ src/components/Resume.tsx
@@ -0,0 +1,5 @@
+const Resume = () => {
+  return <iframe src="/CV_KG_Website.pdf" width="80%" height="100%"></iframe>;
+};
+
+export default Resume;
--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -1,6 +1,7 @@
 import { forwardRef, use } from "react";
 import { useSelector } from "react-redux";
 import { selectSkillsList } from "../store/mainSlice";
+import Resume from "./Resume";
 
 import SkillBox from "./SkillBox";
 
@@ -19,6 +20,9 @@ const Work = forwardRef<HTMLDivElement>((props, ref) => {
       <div className="w-full p-5 pb-4">
         <h2 className="text-xl font-extrabold">What I do</h2>
       </div>
+
+      {/* <Resume></Resume> */}
+
       <div className="flex justify-center items-center p-5 pt-0 w-full">
         <div className="w-full grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
           {skills.map((item: SkillItem) => (
@@ -34,6 +38,21 @@ const Work = forwardRef<HTMLDivElement>((props, ref) => {
             </div>
           ))}
         </div>
+      </div>
+
+      <div className="font-nunito pl-5">
+        For a more detailed look at my skills and experience, feel free to check
+        out my{" "}
+        <span className="cursor-pointer hover:underline font-bold">
+          <a
+            href="https://drive.google.com/file/d/1jmAhqchoWilZGeD0z68yB20lUvDR25gS/view?usp=drive_link"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            resume
+          </a>
+        </span>
+        .
       </div>
     </div>
   );
