cefdca746df19bb29b77c80e9e39ed69140c8fa1
Kenan Garayev <kenangarayev@gmail.com>
2024-05-05 20:16:34 +0100

    Update Work.tsx

--- src/components/Work.tsx
+++ src/components/Work.tsx
@@ -45,7 +45,7 @@ const Work = forwardRef<HTMLDivElement>((props, ref) => {
         out my{" "}
         <span className="cursor-pointer hover:underline font-bold">
           <a
-            href="https://drive.google.com/file/d/1je2o1Bf71MjMNimpy0V75_dGNtdZPZzk/view?usp=sharing"
+            href="https://drive.google.com/file/d/1TU9gmFT0g_rQ5PchMbGPlgaAFdIuUY5u/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
           >
